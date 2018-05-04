bd8bf59fb1903e4a13679ff11fa26433ad8b10f0
rafasaurus <rafa.grigorian@gmail.com>
2018-05-04 22:58:25 +0400

    general

---
+++ install.sh
@@ -0,0 +1,25 @@
+sudo apt-get install -y  build-essential checkinstall cmake pkg-config yasm \
+git \
+gfortran \
+libjpeg8-dev libjasper-dev libpng12-dev \
+
+#ubuntu 14.04
+#libtiff4-dev \
+#ubuntu 16.04
+libtiff5-dev \
+
+libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev \
+libxine2-dev libv4l-dev \
+libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev \
+qt5-default libgtk2.0-dev libtbb-dev \
+libatlas-base-dev \
+libfaac-dev libmp3lame-dev libtheora-dev \
+libvorbis-dev libxvidcore-dev \
+libopencore-amrnb-dev libopencore-amrwb-dev \
+x264 v4l-utils \
+
+libprotobuf-dev protobuf-compiler \
+libgoogle-glog-dev libgflags-dev \
+libgphoto2-dev libeigen3-dev libhdf5-dev doxygen \
+
+sudo apt-get update
