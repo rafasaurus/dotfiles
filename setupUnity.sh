45f248c27749c567a56611fb9debbef5b7226dac
rafasaurus <rafa.grigorian@gmail.com>
2018-05-20 14:02:02 +0400

    unityreset

--- setupUnity.sh
+++ setupUnity.sh
@@ -9,3 +9,13 @@ sudo apt-get install -y htop \
 	numix-gtk-theme \
 	unity-tweak-tool \
 	compizconfig-settings-manager \
+
+# telegram
+sudo add-apt-repository -y ppa:atareao/telegram
+sudo apt-get update
+sudo apt-get install telegram
+
+# papirus icons
+sudo add-apt-repository -y ppa:papirus/papirus
+sudo apt-get update
+sudo apt-get install papirus-icon-theme
