ef4272d175912f177c53eb1fa37463e8acf40d16
rafasaurus <rafa.grigorian@gmail.com>
2018-05-20 14:48:34 +0400

    xps bluetooth.sh added

---
+++ bluetooth_xps_install.sh
@@ -0,0 +1,5 @@
+git clone https://aur.archlinux.org/bcm4350-firmware.git
+cd bcm4350-firmware/
+sudo mv BCM4350C5_003.006.007.0095.1703.hcd /lib/firmware/brcm/BCM-0a5c-6412.hcd
+cd ../
+rm -rf bcm4350-firmware
