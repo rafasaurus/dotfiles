1e15919d3b14629d6f84b62e4c70c31cc585bb3b
rafasaurus <rafa.grigorian@gmail.com>
2018-05-29 16:36:32 +0400

    plank

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get install -y curl vim exuberant-ctags git ack-grep python-pip
-sudo pip install pep8 flake8 pyflakes isort yapf
+sudo pip3 install pep8 flake8 pyflakes isort yapf
