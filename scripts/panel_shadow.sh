7221c2209cbdd073bf0d756767e4fc7b71f49422
rafasaurus <rafa.grigorian@gmail.com>
2018-10-13 16:49:38 +0400

    cleanup

--- scripts/panel_shadow.sh
+++ scripts/panel_shadow.sh
@@ -1,2 +1,2 @@
 sudo mv /usr/share/unity/icons/panel_shadow.png /usr/share/unity/icons/panel_shadow.png.bak
-sudo cp /home/rafael/Dropbox/linuxStuff/panel_shadow.png  /usr/share/unity/icons/
+sudo cp panel_shadow.png  /usr/share/unity/icons/
