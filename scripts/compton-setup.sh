71fd03514c34035d98a4a19e99d5cbd011abea57
rafasaurus <rafa.grigorian@gmail.com>
2019-06-13 22:47:28 +0400

    Add compton i3status setups

---
+++ scripts/compton-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo apt install -y libxcomposite-dev libxdamage-dev libxrandr-dev libxinerama-dev libconfig-dev libdbus-1-dev libglx0 libglu1-mesa-dev freeglut3-dev mesa-common-dev
+git clone https://github.com/chjj/compton
+cd compton 
+sudo make install -j4
