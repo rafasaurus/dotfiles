5f8ffbd8c85769e36a1df49b0d576d73f5c9d580
rafasaurus <rafa.grigorian@gmail.com>
2019-06-29 17:48:57 +0400

    Update caps_state.sh and i3 accordingly

--- scripts/caps_state.sh
+++ scripts/caps_state.sh
@@ -5,5 +5,8 @@ if [ "$state" = "on" ];
 then 
     xdotool key Caps_Lock
     resetXmodmap.sh
+    # ~/.scripts/setxkmap.sh
+    setxkbmap -layout 'us' && xset r rate 240 50
     xmodmap.sh
+    echo "setting xmodmap and setxkmap"
 fi
