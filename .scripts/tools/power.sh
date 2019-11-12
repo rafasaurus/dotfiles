dd5d8865bfa0625f3aed5b5928ba0077fd3b51ec
rafasaurus <rafa.grigorian@gmail.com>
2019-11-12 09:29:28 +0400

    Update power.sh

--- .scripts/tools/power.sh
+++ .scripts/tools/power.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 choices="logout\nshutdown\nsuspend\nlock"
 
-chosen=$(echo -e "$choices" | dmenu -i -p "Choose power")
+chosen=$(echo -e "$choices" | dmenu -i -p "Choose what to do ?")
 case "$chosen" in
     logout) i3-msg exit;;
     lock) betterlockscreen --lock blur -r 1280x720;;
-    shutdown) ~/.scripts/tools/prompt "Do you want to shutdown ?" shutdown now;;
+    shutdown) ~/.scripts/tools/prompt "Do you want to shutdown ?" "shutdown now";;
     suspend) betterlockscreen -s --lock blur;;
 esac
