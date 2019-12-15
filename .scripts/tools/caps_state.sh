d9dbf3fdb6b9ded2dc79be97a1513116c65eca9f
rafasaurus <rafa.grigorian@gmail.com>
2019-12-15 18:10:48 +0400

    Update general

--- .scripts/tools/caps_state.sh
+++ .scripts/tools/caps_state.sh
@@ -4,9 +4,9 @@ state="$(xset q | grep Caps | awk 'BEGIN{FS=" "} {print  $4}')"
 if [ "$state" = "on" ];
 then 
     xdotool key Caps_Lock
-    resetXmodmap.sh
+    ~/.scripts/tools/resetXmodmap.sh
     # ~/.scripts/setxkmap.sh
     setxkbmap -layout 'us' && xset r rate 240 50
-    xmodmap.sh
-    echo "setting xmodmap and setxkmap"
+    ~/.scripts/tools/xmodmap.sh
+    notify-send "setting xmodmap and setxkmap"
 fi
