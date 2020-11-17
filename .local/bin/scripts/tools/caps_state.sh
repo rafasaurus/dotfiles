1f5ab7d4a15b68c77122abf8f32acd886a03f4f6
rafasaurus <rafa.grigorian@gmail.com>
2020-11-17 17:45:16 +0400

    Update caps_state.sh working properly with xmodmap

--- .local/bin/scripts/tools/caps_state.sh
+++ .local/bin/scripts/tools/caps_state.sh
@@ -4,9 +4,9 @@ state="$(xset q | grep Caps | awk 'BEGIN{FS=" "} {print  $4}')"
 if [ "$state" = "on" ];
 then 
     xdotool key Caps_Lock
-    ~/.scripts/tools/resetXmodmap.sh
-    # ~/.scripts/setxkmap.sh
-    setxkbmap -layout 'us' && xset r rate 240 50
-    ~/.scripts/tools/xmodmap.sh
+    setxkbmap -layout 'us' && notify-send "lng english"
+    xmodmap ~/.Xmodmap
     notify-send "setting xmodmap and setxkmap"
+else
+    xdotool key Mode_switch
 fi
