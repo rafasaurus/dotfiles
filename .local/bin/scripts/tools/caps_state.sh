305deb1997364686a8604cc0e5b15e3bf8b51cdc
rafasaurus <rafaelkgrigoryan@gmail.com>
2021-09-25 13:47:20 +0400

    Upgrade fundamentally caps_state.sh

--- .local/bin/scripts/tools/caps_state.sh
+++ .local/bin/scripts/tools/caps_state.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-# requires xdotool package: default in Arch Linux
+#!/bin/sh
+# This scripts disables caps lock if it is on
 state="$(xset q | grep Caps | awk 'BEGIN{FS=" "} {print  $4}')"
 if [ "$state" = "on" ];
 then 
     xdotool key Caps_Lock
-    setxkbmap -layout 'us' && notify-send "lng english"
-    xmodmap ~/.Xmodmap
-    notify-send "setting xmodmap and setxkmap"
-else
-    xdotool key Mode_switch
 fi
