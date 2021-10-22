5a836a86221feb9d17e2d8d1f2bc1978a86305af
rafasaurus <rafaelkgrigoryan@gmail.com>
2021-10-23 00:49:39 +0400

    Update screen layout and power scripts

--- .screenlayout/4k.sh
+++ .screenlayout/4k.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output eDP-1 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
