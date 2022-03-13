73f8fcff77f760e97c88c369922a89517099b341
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-03-13 14:21:49 +0400

    Update repo, add new rofi launcher

--- .screenlayout/built-in.sh
+++ .screenlayout/built-in.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+[ -f ~/.config/wall.png ] && xwallpaper --zoom ~/.config/wall.png || xwallpaper --zoom ~/.config/default_wall.jpg &
