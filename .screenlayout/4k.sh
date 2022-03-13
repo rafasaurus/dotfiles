6e6032cd991ac82a93752a6725cb302c6be33200
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-03-13 14:21:49 +0400

    Update repo, add new rofi launcher

--- .screenlayout/4k.sh
+++ .screenlayout/4k.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off || xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
 [ -f ~/.config/wall.png ] && xwallpaper --zoom ~/.config/wall.png || xwallpaper --zoom ~/.config/default_wall.jpg &
-
-
-
