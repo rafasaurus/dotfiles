a8e09436a661279a1a08dd6088f212ab3aa4ef05
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-08-04 13:11:17 +0400

    Update xscreen scripts

--- .screenlayout/4k.sh
+++ .screenlayout/4k.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
+xrandr --output eDP-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
 [ -f ~/.config/wall.png ] && xwallpaper --zoom ~/.config/wall.png || xwallpaper --zoom ~/.config/default_wall.jpg &
+pkill picom
+picom -b --experimental-backends --blur-method dual_kawase --blur-strength 1
