89a6f55afb88248c5b5fe4a2b2479bc97de335bd
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-08-13 21:33:41 +0400

    Add fzf-tmux

--- .local/bin/fzf-tmux/fzf-tmux.sh
+++ .local/bin/fzf-tmux/fzf-tmux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 DIR="${0%/*}"
-SELECTED="$(find "$DIR" -maxdepth 1 -type f -exec basename {} \; | sort | grep '^_' | sed 's@\.@ @g' | column -s ',' -t |  fzf -e -i --delimiter _ --with-nth='2..' --prompt="fzf-speed: " --info=default --layout=reverse --tiebreak=index | cut -d ' ' -f1)"
-# SELECTED="$(find "$DIR" -maxdepth 1 -type f -exec basename {} \; |  fzf -e -i --delimiter _ --with-nth='2..' --prompt="fzf-speed: " --info=default --layout=reverse --tiebreak=index | cut -d ' ' -f1)"
+SELECTED="$(find "$DIR" -maxdepth 1 -type f -exec basename {} \; | sort | grep '^_' | sed 's@\.@ @g' | column -s ',' -t |  fzf -e -i --delimiter _ --with-nth='2..' --prompt="fzf-tmux: " --info=default --layout=reverse --tiebreak=index | cut -d ' ' -f1)"
+# SELECTED="$(find "$DIR" -maxdepth 1 -type f -exec basename {} \; |  fzf -e -i --delimiter _ --with-nth='2..' --prompt="fzf-tmux: " --info=default --layout=reverse --tiebreak=index | cut -d ' ' -f1)"
 notify-send "$SELECTED"
 $DIR/$SELECTED
 eval "${DIR}/${SELECTED},*"
