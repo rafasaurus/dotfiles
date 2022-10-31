07ff05231979c1c312eb29cb76b54fd476a38e1f
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-10-31 09:47:57 +0400

    apply hald clut all

--- .local/bin/apply-hald-clut.sh
+++ .local/bin/apply-hald-clut.sh
@@ -7,18 +7,34 @@ if [[ $1 == "" ]] #Where "$1" is the positional argument you want to validate
 
 fi
 # for videos
-LPATH="/home/rafael/github/home_sweet_home/scripts/sailfish/Pictures/Camera/512x512/"
-LUTS="dehancer-fuji-astia-100f.png dehancer-fuji-astia-100.png dehancer-fuji-c200.png dehancer-fuji-eterna-vivid-500.png dehancer-fuji-fp100c.png dehancer-fuji-industrial-100.png dehancer-fuji-industrial-400.png dehancer-fuji-natura-1600.png dehancer-fuji-pro-400h.png dehancer-fuji-provia-100f.png dehancer-fuji-provia-400x.png dehancer-fuji-sensia-400.png dehancer-fuji-superia-1600.png dehancer-fuji-superia-200.png dehancer-fuji-velvia-100.png dehancer-fuji-velvia-50.png dehancer-ilford-hp5plus-400.png dehancer-ilford-xp2super-400.png dehancer-kodak-ektar-100.png dehancer-orwo-ut18-exp91.png dehancer-orwo-ut21-exp92.png vsco-fuji-pro-160c.png vsco-fuji-pro-160s.png vsco-fuji-pro-400h.png vsco-fuji-pro-800z.png vsco-fuji-provia-400x.png vsco-fuji-superia-100.png vsco-fuji-t64.png vsco-fuji-velvia-50.png vsco-kodachrome25.png vsco-kodak-portra-160.png vsco-kodak-portra-400.png"
+LPATH="/home/rafael/.local/bin/512x512/"
+ALL_LUTS="dehancer-fuji-astia-100f.png dehancer-fuji-astia-100.png dehancer-fuji-c200.png dehancer-fuji-eterna-vivid-500.png dehancer-fuji-fp100c.png dehancer-fuji-industrial-100.png dehancer-fuji-industrial-400.png dehancer-fuji-natura-1600.png dehancer-fuji-pro-400h.png dehancer-fuji-provia-100f.png dehancer-fuji-provia-400x.png dehancer-fuji-sensia-400.png dehancer-fuji-superia-1600.png dehancer-fuji-superia-200.png dehancer-fuji-velvia-100.png dehancer-fuji-velvia-50.png dehancer-ilford-hp5plus-400.png dehancer-ilford-xp2super-400.png dehancer-kodak-ektar-100.png dehancer-orwo-ut18-exp91.png dehancer-orwo-ut21-exp92.png vsco-fuji-pro-160c.png vsco-fuji-pro-160s.png vsco-fuji-pro-400h.png vsco-fuji-pro-800z.png vsco-fuji-provia-400x.png vsco-fuji-superia-100.png vsco-fuji-t64.png vsco-fuji-velvia-50.png vsco-kodachrome25.png vsco-kodak-portra-160.png vsco-kodak-portra-400.png"
 
-LUTS=vsco-kodachrome25.png
+# LUTS=vsco-kodachrome25.png
 # LUTS=vsco-fuji-velvia-50.png
 # LUTS=vsco-fuji-provia-400x.png
-LUTS="dehancer-fuji-sensia-400.png\nvsco-fuji-pro-400h.png\nvsco-kodachrome25.png\0"
-LUTS=$(echo -e "$LUTS" |  dmenu -i -p "Choose screen option" -l 15)
+# LUTS=dehancer-fuji-sensia-400.png
 
+# choose dialog
+LUTS="`echo $ALL_LUTS | tr ' ' '\n'`"
+LUTS=$(echo -e "$LUTS" |  dmenu -i -p "Choose screen option" -l 15)
 
 CONVERT=/usr/bin/convert
 FL="${1%.*}"
+
+if [ "$LUTS" == "all" ]; then
+    # create new folder, and put all edited images into newly created folder
+    foldername=${1%.*}
+    mkdir $foldername && echo "creating directory $imagename"
+    LUTS=$ALL_LUTS
+    echo $LUTS
+    for LUT in $LUTS
+    do
+      FLLUT="${LUT%.*}"
+      # $CONVERT $1 ${LPATH}/${LUT} -hald-clut $foldername/${FL}-${FLLUT}.jpg && echo "done $LUT"
+    done
+    exit
+fi
 
 for LUT in $LUTS
 do
