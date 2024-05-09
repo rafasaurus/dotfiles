f0f144fc2caa68dad2ca3c9cb0997eedfc59512e
rafasaurus <rafaelkgrigoryan@gmail.com>
2024-05-09 12:20:39 +0400

    Update monitors

--- .screenlayout/built-in.sh
+++ .screenlayout/built-in.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 $HOME/.screenlayout/clear-virmon.sh
 $HOME/.screenlayout/laptop_screen.sh # generate with arandr
+
 [ -f ~/.config/wall.png ] && xwallpaper --zoom ~/.config/wall.png || xwallpaper --zoom ~/.config/default_wall.jpg &
 notify-send "built in display connected" -u critical
 exit 0
