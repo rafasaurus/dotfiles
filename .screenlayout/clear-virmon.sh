b3090c5e81eb26dc7c6f7c32056fb8a122f50059
rafasaurus <rafaelkgrigoryan@gmail.com>
2024-05-12 14:30:46 +0400

    Update sxhkdrc for monitors

--- .screenlayout/clear-virmon.sh
+++ .screenlayout/clear-virmon.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # This script deletes all virtual monitor created by virmon.sh
 #
-if [[ $(xrandr --listactivemonitors | grep VP) ]]; then
-    for display in $(xrandr --listactivemonitors | awk '{print $2}' | grep VP); do
-        echo "killing virtual monitor $display"
-        xrandr --delmonitor $display
-    done
-else 
-    echo "nothing to do"
-fi
+
+xrandr --delmonitor VP-1-1
+xrandr --delmonitor VP-1-2
+xrandr --delmonitor VP-1-3
+
+# if [[ $(xrandr --listactivemonitors | grep VP) ]]; then
+#     for display in $(xrandr --listactivemonitors | awk '{print $2}' | grep VP); do
+#         echo "killing virtual monitor $display"
+#         xrandr --delmonitor $display
+#     done
+# else 
+#     echo "nothing to do"
+# fi
