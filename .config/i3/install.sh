270c7cb86f44f07d199b17aa2c56cbc185a9385e
rafasaurus <rafaelkgrigoryan@gmail.com>
2024-08-21 14:46:47 +0400

    Cleanup

--- .config/i3/install.sh
+++
@@ -1,9 +0,0 @@
-# for volume controls
-playctrl
-# https://github.com/acrisci/playerctl/releases
-sudo apt install feh maim compton
-# for gaps
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev xutils-dev autoconf
-# for multiple monitors
-sudo apt install arandr
-
