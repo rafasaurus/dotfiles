12911d8136ba2fe70b5fa3507d59dc37ff6dad62
rafasaurus <rafaelkgrigoryan@gmail.com>
2024-08-22 12:13:38 +0400

    Renames

--- .local/bin/apply-hald-clut.sh
+++ .local/bin/apply-hald-clut.sh
@@ -15,12 +15,11 @@ fi
 PACK=$(echo -e "$(echo "new old" | tr ' ' '\n')" |  dmenu -i -p "Choose lut pack" -l 15)
 if [ "$PACK" == "new" ]; then
     LPATH="/home/rafael/.local/bin/luts/"
-    ALL_LUTS="dehancer-adox-impolsion-kodak-paper.png dehancer-adox-impolsion.png dehancer-agfa-100.png dehancer-ektar-25-exp-1996+k2383.png dehancer-fujichrome-velvia-100.png dehancer-fujichrome-velvia-50+1ev.png dehancer-fujichrome-velvia-50-1ev.png dehancer-fujichrome-velvia-50-k2383.png dehancer-fuji-eterna-vivid-500-exp-2013-f3513.png dehancer-fuji-eterna-vivid-500-expired-2013-k2383.png dehancer-fuji-eterna-vivid-500.png dehancer-fuji-fp100c-digital-intermediate.png dehancer-fuji-fp100c.png dehancer-fuji-instax-k2383.png dehancer-fuji-reala-500d-exp-2013-k2383.png dehancer-kodachrome-64+3ev.png dehancer-kodachrome-64-3ev.png dehancer-kodachrome-64.png dehancer-kodak-aerocolor-IV-125.png dehancer-kodak-eastman-double-X-5222.png dehancer-kodak-ektar-25-exp-1996+endura.png dehancer-kodak-gold-200.png dehancer-kodak-portra-400+endura.png dehancer-kodak-supra-100+endura.png dehancer-kodak-supra-100+k2383.png dehancer-kodak-vision-3-500t+k2383.png dehancer-konica-minolta-vx400-super.png dehancer-lomochrome-metropolis-xr-100-400.png dehancer-lomochrome-purple.png dehancer-orwo-chrom-ut21-daylight-exp-1992.png dehancer-polachrome-35mm-exp-1986.png dehancer-prokudin-gorskiy-1906.png dehancer-rollei-ortho-25.png dehancer-sverna-type-42-exp-1991.png dehancer-xp2-super400.png"
 else
     LPATH="/home/rafael/.local/bin/512x512/"
-    ALL_LUTS="dehancer-fuji-astia-100f.png dehancer-fuji-astia-100.png dehancer-fuji-c200.png dehancer-fuji-eterna-vivid-500.png dehancer-fuji-fp100c.png dehancer-fuji-industrial-100.png dehancer-fuji-industrial-400.png dehancer-fuji-natura-1600.png dehancer-fuji-pro-400h.png dehancer-fuji-provia-100f.png dehancer-fuji-provia-400x.png dehancer-fuji-sensia-400.png dehancer-fuji-superia-1600.png dehancer-fuji-superia-200.png dehancer-fuji-velvia-100.png dehancer-fuji-velvia-50-new-new.png dehancer-fuji-velvia-50-new.png dehancer-fuji-velvia-50.png dehancer-ilford-hp5plus-400.png dehancer-ilford-xp2super-400.png dehancer-kodak-ektar-100.png dehancer-orwo-ut18-exp91.png dehancer-orwo-ut21-exp92.png vsco-fuji-pro-160c.png vsco-fuji-pro-160s.png vsco-fuji-pro-400h.png vsco-fuji-pro-800z.png vsco-fuji-pro-800z-new.png vsco-fuji-provia-400x.png vsco-fuji-superia-100.png vsco-fuji-t64.png vsco-fuji-velvia-50.png vsco-kodachrome25.png vsco-kodachrome25-new.png vsco-kodak-portra-160.png vsco-kodak-portra-400.png dehancer-adox-color-implosion.png dehancer-adox-color-implosion-grain.png"
 fi
 
+ALL_LUTS="$(ls $LPATH)"
 # choose dialog
 LUTS="`echo $ALL_LUTS | tr ' ' '\n'`"
 LUTS=$(echo -e "$LUTS" |  dmenu -i -p "Choose screen option" -l 15)
