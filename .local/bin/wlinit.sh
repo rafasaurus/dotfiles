a5ecdc782b7f0c0385c1a8916f1b0af03595ddaa
rafasaurus <rafaelkgrigoryan@gmail.com>
2025-05-16 11:56:55 +0400

    Update wayland configs

---
+++ .local/bin/wlinit.sh
@@ -0,0 +1,25 @@
+# example https://github.com/FluffyJay1/dots/blob/master/.scripts/wlinit.sh
+
+pipewire & \
+pipewire-pulse & \
+wireplumber &
+dwlmsg -w > /tmp/dwl_info & \
+yambar & \
+swaybg -i /home/rafael/.config/wall.png & \
+dunst & \
+wmbubble & \
+dropbox & \
+syncthing --no-browser & \
+surf hackernews.com & \
+bluetoothctl power on & \
+notify-send "Welcome to Rice of rafasaurus" & \
+/usr/lib/xdg-desktop-portal & \
+/usr/lib/xdg-desktop-portal-gtk & \
+/usr/lib/xdg-desktop-portal-wlr & \
+wlr-randr --output eDP-1 --mode 1920x1200@120.000000Hz & \
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & \
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & \
+dbus-update-activation-environment --systemd --all & \
+systemctl --user import-environment QT_QPA_PLATFORMTHEME & \
+/usr/libexec/polkit-gnome-authentication-agent-1 & \
+swayidle -w timeout 300 "swaylock -f -c 000000" before-sleep "swaylock -f -c 000000" & 
