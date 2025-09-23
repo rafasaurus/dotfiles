16b6afa954712afd1bed081a806dca3ae387dc99
rafasaurus <rafaelkgrigoryan@gmail.com>
2025-09-23 12:22:33 +0400

    Add full screen screenshots

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-grim -t png -g "$(slurp)" - | tee "$HOME/Pictures/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png" | wl-copy --type image/png
+case "$1" in
+  -h|--help) echo "Usage: $(basename "$0") [-s|--select]"; exit;;
+  -s|--select) S="$(slurp)" || exit; set -- -g "$S";;
+esac
+D="$HOME/Pictures/screenshots"
+mkdir -p "$D"
+F="$D/$(date +%F_%H-%M-%S).png"
+grim -t png "$@" - | tee "$F" | wl-copy --type image/png
