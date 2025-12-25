cecf357acb887c6fdb73649f1d1b4da07a029070
rafasaurus <rafaelkgrigoryan@gmail.com>
2025-12-25 20:19:15 +0400

    Update wlinit

--- .local/bin/wlinit.sh
+++ .local/bin/wlinit.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 # example https://github.com/FluffyJay1/dots/blob/master/.scripts/wlinit.sh
 
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME & \
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP & \
-dbus-update-activation-environment --systemd --all & \
-pipewire & \
-pipewire-pulse & \
-wireplumber & \
-dwlb -ipc & \
-dwlb-status | dwlb -status-stdin all & \
-swaybg -i /home/rafael/.config/wall.png & \
-dunst & \
-syncthing --no-browser & \
-surf hackernews.com & \
-bluetoothctl power on & \
-notify-send "Welcome to Rice of rafasaurus" & \
-wlr-randr --output eDP-1 --mode 1920x1200@120.000000Hz & \
-# /usr/lib/xdg-desktop-portal-gtk & \
-# /usr/lib/xdg-desktop-portal-wlr & \
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+dbus-update-activation-environment --systemd --all &
+killall pipewire pipewire-pulse wireplumber syncthing
+pipewire &
+pipewire-pulse &
+wireplumber &
+syncthing --no-browser &
+dwlb -ipc &
+dwlb-status | dwlb -status-stdin all &
+swaybg -i $HOME/.config/wall.png &
+dunst &
+surf hackernews.com &
+bluetoothctl power on &
+wlr-randr --output eDP-1 --mode 1920x1200@120.000000Hz &
+notify-send "Welcome to Rice of rafasaurus" &
 
-# /usr/libexec/polkit-gnome-authentication-agent-1 & \
+# /usr/lib/xdg-desktop-portal-gtk &
+# /usr/lib/xdg-desktop-portal-wlr &
+# /usr/libexec/polkit-gnome-authentication-agent-1 &
