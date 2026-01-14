66928b25918cef8c2c04b3d02eafd2a89e8202be
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-01-15 00:36:47 +0400

    Refactor, remove x11 startup.sh, and other utils

--- .local/bin/caps_state.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This scripts disables caps lock if it is on
-state="$(xset q | grep Caps | awk 'BEGIN{FS=" "} {print  $4}')"
-if [ "$state" = "on" ];
-then 
-    xdotool key Caps_Lock
-fi
