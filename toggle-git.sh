bfa279e300ae91665052137b1fdaf3b570927be9
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-04-22 12:48:20 +0400

    Update toggle-git.sh

--- toggle-git.sh
+++ toggle-git.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 # Toggles git submodule urls from https to ssh or vice versa in .gitmodules
+# Skips private-fonts: it requires SSH regardless of mode
 
 FILE=".gitmodules"
 
 [ ! -f "$FILE" ] && exit 1
 
+PRIVATE="rafasaurus/private-fonts"
+
 if grep -q "https://github.com/" "$FILE"; then
-    sed -i 's|url = https://github.com/|url = git@github.com:|g' "$FILE"
+    sed -i "\|url.*$PRIVATE|! s|url = https://github.com/|url = git@github.com:|g" "$FILE"
     echo "Changed all to git"
 else
-    sed -i 's|url = git@github.com:|url = https://github.com/|g' "$FILE"
+    sed -i "\|url.*$PRIVATE|! s|url = git@github.com:|url = https://github.com/|g" "$FILE"
     echo "Changed all to https"
 fi
 
