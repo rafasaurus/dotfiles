4472456ce15dc48bc9b59d7b928a9a088c3a97f6
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-07-24 15:53:52 +0400

    Change fonts.conf, statusbar font

--- .local/bin/relaunch_statusbar.sh
+++ .local/bin/relaunch_statusbar.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 killall -9 dwlb dwlb-status
-dwlb -ipc -font "monospace:italic:size=11.5" &
+# dwlb -ipc -font "Ttyp0:italic:size=13" &
+dwlb -ipc -font "terminus:semibold:size=13:antialias=false" &
 dwlb-status | dwlb -status-stdin all &
