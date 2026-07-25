78a373ccbeb07c75ff4db56dbd5c18adfda27c05
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-07-25 20:56:27 +0400

    Update lock screen, only clouds

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-swaylock -e -F -k -f -c 000000 -i "$(find ~/.config/patterns/ -type f | shuf -n 1)" -s tile
+# swaylock -e -F -k -f -c 000000 -i "$(find ~/.config/patterns/ -type f | shuf -n 1)" -s tile
+swaylock -e -F -k -f -c 000000 -i "$(ls ~/.config/patterns/cloud* | shuf -n 1)" -s tile
