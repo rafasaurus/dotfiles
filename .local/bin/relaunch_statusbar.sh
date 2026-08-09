30ae35ca2dc4275e41ed6c8515180772baf6e042
rafasaurus <rafaelkgrigoryan@gmail.com>
2026-08-09 15:10:42 +0400

    Tttyp0 for statusbar

--- .local/bin/relaunch_statusbar.sh
+++ .local/bin/relaunch_statusbar.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 killall -9 dwlb dwlb-status
-# dwlb -ipc -font "Ttyp0:italic:size=13" &
-dwlb -ipc -font "terminus:semibold:size=13:antialias=false" &
+dwlb -ipc -font "Ttyp0 OTB:size=11.5:antialias=false" &
 dwlb-status | dwlb -status-stdin all &
